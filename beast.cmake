f18c56c8214c96fd5304fa47160120039b5dd3b6
André Rocha <d3roch4@gmail.com>
2017-10-06 10:09:28 -0300

    Add files via upload

---
+++ beast.cmake
@@ -0,0 +1,20 @@
+
+if(NOT TARGET beast)
+    include(ExternalProject)
+
+    ExternalProject_Add (
+        beast
+        #URL  "https://github.com/boostorg/beast/archive/master.zip"
+        URL  "file://${CMAKE_SOURCE_DIR}/deps/beast-master.tgz"
+        BUILD_COMMAND ""
+        BUILD_IN_SOURCE 1
+        INSTALL_COMMAND cmake -E echo "Skipping install step."
+    )
+    ExternalProject_Get_Property(beast source_dir binary_dir)
+
+    set(BEAST_INCLUDE "${source_dir}/include"
+            CACHE INTERNAL "${PROJECT_NAME}: Include Directories" FORCE)
+endif()
+
+add_dependencies(${PROJECT_NAME} beast)
+include_directories(${BEAST_INCLUDE})
